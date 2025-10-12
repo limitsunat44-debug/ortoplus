f740b9e5bea045f0cecf226fa9bc5a21eb217a60
limitsunat44-debug <limitsunat44@gmail.com>
2025-10-12 19:25:16 +0300

    Update admin-script.js

--- admin-script.js
+++ admin-script.js
@@ -7,8 +7,8 @@ const ADMIN_CREDENTIALS = {
 // НОВАЯ КОНФИГУРАЦИЯ для синхронизации данных между устройствами
 const CLOUD_SYNC_CONFIG = {
     // Используем JSONBin.io для бесплатного облачного хранилища
-    binId: '68e78e8aae596e708f0bb24e', // ID для хранилища пользователей (тот же что в user-script)
-    apiKey: '$2a$10$YC7NQGItIeryJjaqSp/7oes7nTxC1YKQ9M3VVomFXDImvTlKn6QUm', // Ваш API ключ
+    binId: '68ebc131d0ea881f409f1d60', // ID для хранилища пользователей (тот же что в user-script)
+    apiKey: '$2a$10$KN9N1PK8kpK3RnH46uDtY.XRG51JVUumStqkC0lETjQcZU7tJX/7K', // Ваш API ключ
     baseUrl: 'https://api.jsonbin.io/v3/b',
     enabled: true // Включить синхронизацию с облаком
 };
